92a0833b66fcac906bf411437066a1fb8ca9b381
Murat Yirci <myirci@gmail.com>
2016-05-12 18:45:02 +0200

    mod.

--- Chess/move_generator/main.cpp
+++ Chess/move_generator/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <sstream>
 #include <vector>
+#include <array>
 
 enum Color : std::uint8_t {
-    White,
-    Black
+    Black,
+    White
+};
+
+enum Castling : std::uint8_t {
+    King_side,
+    Queen_side
 };
 
 enum Square : std::uint8_t {
@@ -18,9 +25,9 @@ enum Square : std::uint8_t {
     Outside
 };
 
-inline std::uint8_t rank(std::uint8_t s)                    { return s / 8; }
-std::uint8_t file(std::uint8_t s)                           { return s % 8; }
-std::uint8_t sq(std::uint8_t rank_no, std::uint8_t file_no) { return static_cast<std::uint8_t>(8*rank_no + file_no); }
+inline std::uint8_t rank(std::uint8_t s) { return s / 8; }
+inline std::uint8_t file(std::uint8_t s) { return s % 8; }
+inline Square sq(std::uint8_t rank_no, std::uint8_t file_no) { return static_cast<Square>(8*rank_no + file_no); }
 
 std::string square_to_string[64] = {
     "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
@@ -60,7 +67,52 @@ enum Piece : std::int8_t {
     BKing   = -WKing
 };
 
-char piece_to_char(Piece p) {
+inline Piece char_to_piece(char c) {
+
+    switch(c) {
+    case 'P':
+        return WPawn;
+        break;
+    case 'p':
+        return BPawn;
+        break;
+    case 'K':
+        return WKing;
+        break;
+    case 'k':
+        return BKing;
+        break;
+    case 'R':
+        return WRook;
+        break;
+    case 'r':
+        return BRook;
+        break;
+    case 'N':
+        return WKnight;
+        break;
+    case 'n':
+        return BKnight;
+        break;
+    case 'B':
+        return WBishop;
+        break;
+    case 'b':
+        return BBishop;
+        break;
+    case 'Q':
+        return WQueen;
+        break;
+    case 'q':
+        return BQueen;
+        break;
+    default:
+        return Empty;
+        break;
+    }
+}
+
+inline char piece_to_char(Piece p) {
     switch(p) {
     case Empty:
         return '.';
@@ -170,10 +222,9 @@ public:
     // 8-bit additional space is also avaliable!
 };
 
-
 struct Board {
 
-    Board() : en_passant_sq(Outside), captured(Empty) {
+    Board() {
         brd[a1] = brd[h1] = WRook;
         brd[b1] = brd[g1] = WKnight;
         brd[c1] = brd[f1] = WBishop;
@@ -205,6 +256,27 @@ struct Board {
 
     }
 
+    void set_side_to_move(Color c) { (c == White) ? (flags |= 1) : (flags &= 0xFE); }
+
+    Color get_side_to_move() const { return static_cast<Color>(flags & 1); }
+
+    void set_castling_rights(Color clr, Castling cst) {
+        (clr == White)     ? (flags |= 2) : (flags &= 0xFD);
+        (cst == King_side) ? (flags |= 4) : (flags &= 0xFB);
+    }
+
+    bool check_castling_availability(Color clr, Castling cst) const {
+
+    }
+
+    void set_captured_piece(Piece p) {
+
+    }
+
+    Piece get_captured_piece() const {
+
+    }
+
     void print_board() const {
 
         for(int i = 0; i < 64; ++i) {
@@ -217,9 +289,10 @@ struct Board {
 
     Piece brd[64];
     Square en_passant_sq;
-    Piece captured;         // not needed for extended moves
+    std::uint8_t flags; // [Free to use -> 1 bit][Captured piece-> 4 bits] [Castling-> 2 bits] [Color-> 1 bit]
+    std::uint8_t fifty_move_counter;
+    std::uint16_t move_counter;
 };
-
 
 class move_generator {
 public:
@@ -234,8 +307,95 @@ private:
 
 };
 
+
+/*
+FEN: Forsyth-Edwards Notation
+Field-1: Piece Placement
+Field-2: Active Color
+Field-3: Castling Availability
+Field-4: En Passant
+Field-5: Half Move Clock
+Field-6: Full Move Number
+
+Here are some example positions in FEN notation:
+Pos-1: 6k1/pp3pp1/3p4/2pP2p1/4Pp2/8/1P3PP1/6K1 b KQkq c6 1 2
+Pos-2: rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
+Pos-3: r1bq1rk1/4bppp/p3pn2/1pn3B1/8/P1NBPN2/1P3PPP/R2Q1RK1
+*/
+
+class FEN {
+public:
+    FEN() : fen_str{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"} { }
+
+    FEN(const std::string& fen) : fen_str{fen} { }
+
+    void set_fen(const std::string& fen) { fen_str = fen; }
+
+    void set_fen(std::string&& fen)      { fen_str = std::move(fen); }
+
+    const std::string& get_fen() const   {return fen_str; }
+
+    void to_board(Board& b) const {
+
+        std::array<std::string, 6> fields;
+        parse(fields);
+
+        // Field-1: Piece placement
+        std::istringstream is{fields[0]};
+        std::string str{""};
+        for(int i = 0; i < 8; ++i) {
+            std::getline(is, str, '/');
+            int f{-1};
+            for(int j = 0; j < str.length(); ++j) {
+                if(std::isdigit(str[j]))
+                    for(int k = 0; k < int(str[j] - '0'); ++k)
+                        b.brd[sq(7-i, ++f)] = Empty;
+                else b.brd[sq(7-i, ++f)] = char_to_piece(str[j]);
+            }
+        }
+
+        // Field-2: Active color
+        if(fields[1] == "w") b.set_side_to_move(White);
+        else b.set_side_to_move(Black);
+
+        // Field-3: Castling Availability
+
+        // Field-4: En Passant
+        (fields[3] == "-") ? b.en_passant_sq = Outside : b.en_passant_sq = sq(fields[3][1] - '1', fields[3][0] - 'a');
+
+        // Field-5: Half Move Clock
+        b.fifty_move_counter = static_cast<std::uint8_t>(std::stoi(fields[4]));
+
+        // Field-6: Full Move Number
+        b.move_counter = static_cast<std::uint16_t>(std::stoi(fields[5]));
+    }
+
+    void parse(std::array<std::string, 6>& fields) const {
+        std::istringstream ss {fen_str};
+        for(int i = 0; i < 6; ++i)
+            ss >> fields[i];
+    }
+
+private:
+    std::string fen_str;
+};
+
 int main(void) {
 
+    /*
     Board b;
+    std::cout << sizeof(Board) << std::endl;
     b.print_board();
+
+    FEN f("6k1/pp3pp1/3p4/2pP2p1/4Pp2/8/1P3PP1/6K1 b KQkq c6 1 2");
+    f.to_board(b);
+    b.print_board();
+    */
+
+    for(int i = 0; i < 4; ++i) {
+        std::cout << (1 << i) << std::endl;
+    }
+
+
+
 }
